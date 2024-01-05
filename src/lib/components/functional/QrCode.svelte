a07302bff21308969add6207a6c5b4776d3b3595
Andri Schatz <andri.schatz@gmail.com>
2024-01-05 22:05:26 +0100

    add qr

---
+++ src/lib/components/functional/QrCode.svelte
@@ -0,0 +1,20 @@
+<script>
+    import QRCode from 'qrcode';
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
+
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    let canvas;
+
+    onMount(() => {
+        console.log(window.location.href);
+        QRCode.toCanvas(canvas, window.location.href, { width: 200});
+    });
+</script>
+
+<div>
+    <p>QR Code to share this page:</p>
+    <canvas id="qrcode" bind:this={canvas}></canvas>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
     import SamplePlayer from "$lib/components/functional/SamplePlayer.svelte";
     import BlurTainer from "$lib/components/layout/BlurTainer.svelte";
+    import QrCode from "$lib/components/functional/QrCode.svelte";
 </script>
 
 <div class="flex items-center justify-center h-screen">
     <BlurTainer classes="h-3/5 w-3/5 flex flex-col space-y-10 items-center justify-center">
         <SamplePlayer />
+
+        <QrCode />
     </BlurTainer>
 </div>
